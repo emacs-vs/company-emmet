8f0b7f9a5ca2ea0c4d78866f4c59fdb326b8293c
JenChieh <jcs090218@gmail.com>
2025-01-01 01:07:17 -0800

    Update copyright year

--- company-emmet.el
+++ company-emmet.el
@@ -1,6 +1,6 @@
 ;;; company-emmet.el --- Company completion for emmet  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2024  Shen, Jen-Chieh
+;; Copyright (C) 2023-2025 Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
